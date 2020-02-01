bddc7b310e0acd3e7dd6d17a6a45068c13de01ba
Michael De La Rosa <m.delarosa@me.com>
2020-01-31 20:11:37 -0700

    Fix nav icons position

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 var close = document.getElementById('x-button');
 var welcomeBox = document.getElementById('welcome');
+var navIcon = document.querySelector('.nav-link');
+var wallet = document.querySelector('.wallet');
+var profile = document.querySelector('.profile');
 
-close.addEventListener("click", hideBanner)
+close.addEventListener("click", hideBanner);
+wallet.addEventListener("click", highlightWallet);
+profile.addEventListener("click", highlightProfile);
 
 function hideBanner() {
-  console.log("YO!");
   welcomeBox.classList.add('hidden');
+}
+
+function highlightWallet() {
+  wallet.classList.add('selected');
+}
+
+function highlightProfile() {
+  profile.classList.add('selected');
 }
