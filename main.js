9e84f597f6cab9e79c282193e87a0ccd12015217
Michael De La Rosa <m.delarosa@me.com>
2020-01-31 20:59:23 -0700

    Added nav icon border functionality

--- main.js
+++ main.js
@@ -3,19 +3,32 @@ var welcomeBox = document.getElementById('welcome');
 var navIcon = document.querySelector('.nav-link');
 var wallet = document.querySelector('.wallet');
 var profile = document.querySelector('.profile');
+var clipboard = document.querySelector('.clipboard');
+
 
 close.addEventListener("click", hideBanner);
 wallet.addEventListener("click", highlightWallet);
 profile.addEventListener("click", highlightProfile);
+clipboard.addEventListener("click", highlightClipboard);
 
 function hideBanner() {
   welcomeBox.classList.add('hidden');
 }
 
+function highlightProfile() {
+  clipboard.classList.remove('selected');
+  wallet.classList.remove('selected');
+  profile.classList.add('selected');
+}
+
 function highlightWallet() {
+  clipboard.classList.remove('selected');
+  profile.classList.remove('selected');
   wallet.classList.add('selected');
 }
 
-function highlightProfile() {
-  profile.classList.add('selected');
+function highlightClipboard() {
+  wallet.classList.remove('selected');
+  profile.classList.remove('selected');
+  clipboard.classList.add('selected');
 }
